714e3c04826afab6c1d55300d17df433d019da87
Toshiya Kimura <kimukendou0506@icloud.com>
2021-03-13 22:16:07 +0900

    変更した内容をメモ

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(function($) {
+    $('.image-slider').bgswitcher({
+        images: ['../img/a.png','../img/DSC03986.JPG','../img/10.PNG','../img/12.JPG'], // 切替背景画像を指定
+	interval: 3000, // 背景画像を切り替える間隔を指定 3000=3秒
+        loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
+        shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
+        effect: "blind", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
+        duration: 500, // エフェクトの時間を指定します。
+        easing: "swing" // エフェクトのイージングをlinear,swingから指定
+    });
+});
