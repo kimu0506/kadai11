0ad8fc1382176e5bdcd851791970d42b009f5bc0
Toshiya Kimura <kimukendou0506@icloud.com>
2021-03-14 00:14:20 +0900

    変更した内容をメモ

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 $(function($) {
-    $('.image-slider').bgswitcher({
+    $('.bg-slider').bgswitcher({
         images: ['../img/a.png','../img/DSC03986.JPG','../img/10.PNG','../img/12.JPG'], // 切替背景画像を指定
 	interval: 3000, // 背景画像を切り替える間隔を指定 3000=3秒
         loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
